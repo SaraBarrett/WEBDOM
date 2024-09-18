1fcf2e8858391d83811f79e1b3328cac3a3c08ec
SaraBarrett <sara.c.mon@gmail.com>
2024-09-18 12:10:31 +0100

    exs

--- js/script.js
+++ js/script.js
@@ -21,11 +21,22 @@
 // console.log(doneTodos)
 // console.log(checkbox)
 
-let myTextVar = document.querySelector('a');
+// let myTextVar = document.querySelector('a');
 //myTextVar.innerText = 'cucu vamos manipular coisas';
 //myTextVar.innerHTML = 'cucu vamos manipular coisas e trocar elementos';
 
-myTextVar.href = 'www.cesaedigital.pt';
+// myTextVar.href = 'www.cesaedigital.pt';
 
 // let myVar = document.querySelector('span');
 // myVar.innerText = 'yackk';
+
+
+function changeImage(){
+    let myImage = document.querySelector('img');
+
+    if(myImage.src == 'https://devsprouthosting.com/images/chicken.jpg'){
+        myImage.setAttribute('src', 'https://devsprouthosting.com/images/egg.jpg');
+    }else{
+        myImage.setAttribute('src', 'https://devsprouthosting.com/images/chicken.jpg');
+    }
+}

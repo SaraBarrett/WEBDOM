673faaaabcd65e83a1c0c28938fdd3eb316b1ba2
SaraBarrett <sara.c.mon@gmail.com>
2024-09-18 11:38:05 +0100

    exs

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+// let bannerSelection = document.getElementById('banner');
+
+// if(bannerSelection){
+//     console.log(bannerSelection);
+// }
+
+
+// let image = document.getElementById('unicorn');
+// let heading = document.getElementById('mainheading');
+
+
+// console.log(image)
+// console.log(heading)
+
+// let doneTodos = document.querySelectorAll('.done');
+
+// let checkbox = document.querySelector("input[type='checkbox']");
+
+
+// console.log(doneTodos)
+// console.log(checkbox)
+
+let myTextVar = document.querySelector('a');
+//myTextVar.innerText = 'cucu vamos manipular coisas';
+//myTextVar.innerHTML = 'cucu vamos manipular coisas e trocar elementos';
+
+myTextVar.href = 'www.cesaedigital.pt';
+
+// let myVar = document.querySelector('span');
+// myVar.innerText = 'yackk';

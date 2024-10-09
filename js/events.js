7832c6117eaa3a85b6a4741f1be87b50874a206c
SaraBarrett <sara.c.mon@gmail.com>
2024-10-09 15:30:40 +0100

    update

--- js/events.js
+++ js/events.js
@@ -46,32 +46,32 @@
 //     hello('hello')
 // });
 // let myBtn = document.getElementById('myBtn');
-// let btnHello = document.getElementById('hello');
-// let btnGoodbye = document.getElementById('goodbye');
-// let changeColor = document.getElementById('change-color');
-// let newColor = document.getElementById('new-color');
-// let myBg = document.querySelector('#container');
+let btnHello = document.getElementById('hello');
+let btnGoodbye = document.getElementById('goodbye');
+let changeColor = document.getElementById('change-color');
+let newColor = document.getElementById('new-color');
+let myBg = document.querySelector('#container');
 
-// function changeColorF(){
-//     this.style.backgroundColor = generateColor();
-// }
+function changeColorF(){
+    this.style.backgroundColor = generateColor();
+}
 
-// function getRandomInt() {
-//     return Math.floor(Math.random() * 256);
-// }
+function getRandomInt() {
+    return Math.floor(Math.random() * 256);
+}
 
-// function generateColor(){
-//     let myGeneratedColor = 'rgb('+getRandomInt()+','+getRandomInt()+','+getRandomInt()+')'
-//     return myGeneratedColor;
-// }
+function generateColor(){
+    let myGeneratedColor = 'rgb('+getRandomInt()+','+getRandomInt()+','+getRandomInt()+')'
+    return myGeneratedColor;
+}
 
 
 // // btnHello.addEventListener('click', )
 
-// btnHello.addEventListener('click', changeColorF);
-// myBg.addEventListener('click', changeColorF);
-// btnGoodbye.addEventListener('click', changeColorF);
-// changeColor.addEventListener('click', changeColorF);
+btnHello.addEventListener('click', changeColorF);
+myBg.addEventListener('click', changeColorF);
+btnGoodbye.addEventListener('click', changeColorF);
+changeColor.addEventListener('click', changeColorF);
 
 window.addEventListener('keydown', function(event){
     console.log(event.code);
